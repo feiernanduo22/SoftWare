a457fd20a2d3aa6b5718e71fa443495b5d62bb83
feiernanduo22 <feiernanduo22@163.com>
2019-08-26 22:22:18 +0800

    修改代码格式

--- src/com/software/test01/BuyChicken.java
+++ src/com/software/test01/BuyChicken.java
@@ -5,10 +5,14 @@ package com.software.test01;
  * @data 2019/5/27 21:43
  */
 public class BuyChicken {
+
+
     /*算法应用之百钱买白鸡
     案列说明：主要内容是：公鸡5元一只，母鸡3元一只，小鸡1元三只，问100元怎样可以买100鸡？
     思想：想要实现此算法，只要明白各种条件的关系即可，而且知道公鸡最多买20只，母鸡最多买33只，小鸡最多买100只，这样买各种鸡的钱总为100,元，鸡的只数也是100；
     */
+
+
     public static void main(String[] args) {
         final Integer MAXCOCKCOUNT = 20;//公鸡最大数量
         final Integer MAXHENCOUNT = 33;//母鸡最大数量
